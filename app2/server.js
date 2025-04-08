ff1dc524500f17eae433fc9597a6a5bb8e5b740d
Sergey Smolnikov <ssmtor@gmail.com>
2025-04-08 12:10:58 +0200

    c

---
+++ app2/server.js
@@ -0,0 +1,30 @@
+// Import required modules
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Home Page!!
+        routes: /about /contact /services`);
+});
+
+// About Route
+app.get('/about', (req, res) => {
+    res.send('This is the About Page.');
+});
+
+// Contact Route
+app.get('/contact', (req, res) => {
+    res.send('This is the Contact Page.');
+});
+
+// Services Route
+app.get('/services', (req, res) => {
+    res.send('Our Services Page. ${req.url}');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at port ${port}/`);
+});
