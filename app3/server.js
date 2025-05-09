aaa0ae3ae110fab34f552ab6f93ad65d5bcffef9
Sergey Smolnikov <ssmol@equinor.com>
2025-05-09 15:12:07 +0200

    ds

---
+++ app3/server.js
@@ -0,0 +1,41 @@
+// Import required modules
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Home Route
+app.get('/', (req, res) => {
+    res.send(`Welcome to the Home Page!!!
+        routes: /about /contact /services`);
+});
+
+// About Route
+app.get('/about', (req, res) => {
+    res.send('This is the About Page.');
+});
+
+// Contact Route
+app.get('/contact', (req, res) => {
+    res.send('This is the Contact Page.');
+});
+
+// Services Route
+app.get('/services', (req, res) => {
+    res.send('Our Services Page. ${req.url}');
+});
+
+// Services Route
+app.get('/healthz/live', (req, res) => {
+    res.send('Ok /healthz/live');
+});
+
+// Services Route
+app.get('/healthz/ready', (req, res) => {
+    res.send('Ok /healthz/ready');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at port ${port}/`);
+});
+
